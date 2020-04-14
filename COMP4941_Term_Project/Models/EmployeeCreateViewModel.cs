299c11db253e88c6ad6f48e0e0119a024c982f71
Minseok Kong <eoeoms@naver.com>
2020-04-14 03:36:04 -0700

    Input validation for Employee Email and Password during Create operations

---
+++ COMP4941_Term_Project/Models/EmployeeCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP4941_Term_Project.Models
+{
+    public class EmployeeCreateViewModel
+    {
+        public Employee Employee { get; set; }
+        public RegisterViewModel RegisterViewModel { get; set; }
+    }
+}
