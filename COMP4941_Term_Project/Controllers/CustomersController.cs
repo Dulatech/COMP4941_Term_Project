8d7ce5ae81247de61df683151cedb0379f170e4b
Minseok Kong <eoeoms@naver.com>
2020-04-14 03:39:13 -0700

    Fixed Customer Create and Edit

--- COMP4941_Term_Project/Controllers/CustomersController.cs
+++ COMP4941_Term_Project/Controllers/CustomersController.cs
@@ -11,7 +11,20 @@ namespace COMP4941_Term_Project.Controllers
     [CustomActionFilter]
     public class CustomersController : Controller
     {
-        private AppContext db = new AppContext();
+        private AppContext db;
+
+        public CustomersController()
+        {
+            // set DbContext specific to the branch of the logged in user
+            object branchID = System.Web.HttpContext.Current.Session["branch"];
+            if (branchID != null)
+            {
+                if (branchID.ToString() == "admin")
+                    db = new AppContext();
+                else
+                    db = new BranchContext("b-" + branchID);
+            }
+        }
 
         // GET: Customers
         [CustomActionFilter]
@@ -51,31 +64,29 @@ namespace COMP4941_Term_Project.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,BranchID,Picture")] Customer customer,
-            [Bind(Include = "Title, FirstName, MiddleName, LastName, NickName, MaidenName")] FullName name,
-            [Bind(Include = "Street, City, Province, Country, PostalCode")] FullAddress ha)
+        public ActionResult Create(Customer customer, // Input field values of from the client side's html form
+                                   FullName name,     // that are associated with these entities are automatically 
+                                   FullAddress ha)    // binded to these objects ([BindAttribute] is optional)
         {
             if (ModelState.IsValid)
             {
-                customer.ID = Guid.NewGuid();
-                name.ID = Guid.NewGuid();
-                ha.ID = Guid.NewGuid();
-                customer.Name = name;
-                customer.HomeAddress = ha;
-                Guid? branchID = customer.BranchID;
-                // People table references Branch table for FK, but Branch table doesn't exist in the BranchDbContext
-                // which causes INSERT error. Thus BranchID for this employee is set to null in the Branch's DB,
-                // but not in AspNetUsers table
-                customer.BranchID = null;
-                BranchContext branchDb = new BranchContext("b-" + branchID);
-                db.People.Add(customer);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                customer.ID = Guid.NewGuid(); // Create new Guid's for ID,
+                name.ID = Guid.NewGuid();     // FullName's ID,
+                ha.ID = Guid.NewGuid();       // and FullAddress' ID
+                customer.Name = name;         // Set Customer's Name entity to the FullName parameter
+                customer.HomeAddress = ha;    // same for FullAddress
+                // connect to the instance of database specific to this customer's branch
+                BranchContext branchDb = new BranchContext("b-" + customer.BranchID);
+                // Add this customer to DBSet. All the associated values are also inserted to
+                // their corresonding tables automatically as well (People, FullName, and FullAddress)
+                branchDb.Customers.Add(customer);
+                branchDb.SaveChanges(); // save changes to the database
+                return RedirectToAction("Index"); // redirects user to the Index View
             }
-
+            // if ModelState is invalid, sends the same page as response (stays on the same page)
             ViewBag.BranchID = new SelectList(db.Branches, "ID", "Name", customer.BranchID);
             ViewBag.ID = new SelectList(db.FullNames, "ID", "Title", customer.ID);
-            return View(customer);
+            return View(customer); 
         }
 
         // GET: Customers/Edit/5
@@ -86,7 +97,7 @@ namespace COMP4941_Term_Project.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Customer customer = (Customer)db.People.Find(id);
+            Customer customer = db.Customers.Include(c => c.Name).Include(c => c.HomeAddress).SingleOrDefault(c => c.ID == id);
             if (customer == null)
             {
                 return HttpNotFound();
@@ -101,16 +112,22 @@ namespace COMP4941_Term_Project.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,BranchID,Picture")] Customer customer)
+        public ActionResult Edit(Customer customer,
+                                 FullNameEdit name,  // ViewModels
+                                 FullAddressEdit ha) // for editing
         {
             if (ModelState.IsValid)
             {
+                // For name and address, the input values form the client side is binded to a view model,
+                // then those information is used to set the values of the actual name and address object from
+                // the database with the same ID. Then, we set the state of the entities for Customer,
+                // FullName and FullAddress to Modified
+                db.Entry(name.UpdateFullName(db.FullNames.Find(name.fnID))).State = EntityState.Modified;
+                db.Entry(ha.UpdateFullAddress(db.FullAddresses.Find(ha.faID))).State = EntityState.Modified;
                 db.Entry(customer).State = EntityState.Modified;
-                db.SaveChanges();
+                db.SaveChanges(); // save modifications to the database
                 return RedirectToAction("Index");
             }
-            ViewBag.BranchID = new SelectList(db.Branches, "ID", "Name", customer.BranchID);
-            ViewBag.ID = new SelectList(db.FullNames, "ID", "Title", customer.ID);
             return View(customer);
         }
 
@@ -143,7 +160,7 @@ namespace COMP4941_Term_Project.Controllers
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
+            if (disposing && db != null)
             {
                 db.Dispose();
             }
