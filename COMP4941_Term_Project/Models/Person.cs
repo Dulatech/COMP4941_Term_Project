31658a16311c0e1c926f83df7032ebacd53a19b0
Alec martin <acmartin1@live.ca>
2020-04-18 20:22:54 -0700

    BranchID require

--- COMP4941_Term_Project/Models/Person.cs
+++ COMP4941_Term_Project/Models/Person.cs
@@ -12,11 +12,8 @@ namespace COMP4941_Term_Project.Models
     {
       
         public Guid ID { get; set; }
-        [Required]
         public Guid? BranchID { get; set; }
 
-
-        [Required]
         public virtual Branch Branch { get; set; }
 
         public virtual FullName Name { get; set; }
