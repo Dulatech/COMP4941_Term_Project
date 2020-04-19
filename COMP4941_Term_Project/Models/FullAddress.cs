09373d8fecc6967994891347ce69feb81e2c5fb3
Minseok Kong <eoeoms@naver.com>
2020-04-18 20:42:48 -0700

    required fields modified for address model

--- COMP4941_Term_Project/Models/FullAddress.cs
+++ COMP4941_Term_Project/Models/FullAddress.cs
@@ -10,13 +10,10 @@ namespace COMP4941_Term_Project.Models
 {
     public class FullAddress
     {
-      
         public Guid ID { get; set; }
         [DisplayName("Room No")]
-        [Required]
         public string RoomNo { get; set; }
         public string POBox { get; set; }
-        [Required]
         public string Unit { get; set; }
 
         [Range(1,200)]
@@ -36,12 +33,9 @@ namespace COMP4941_Term_Project.Models
         [DisplayName("Postal Code")]
         [Required]
         public string PostalCode { get; set; }
-        [Required]
         public string Cell { get; set; }
-        [Required]
         public string Phone { get; set; }
         public string Fax { get; set; }
-        [Required]
         public string Email { get; set; }
         public virtual Branch Branch { get; set; }
     }
